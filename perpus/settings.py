a76029ce475e3cfcbbbd13798ca69a2a8746f2ad
Sendi Agustian <sendiagustian.pk@gmail.com>
2021-02-11 11:27:47 +0700

    static file

--- perpus/settings.py
+++ perpus/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'perpustakaan/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'perpustakaan/static')
 
 LOGIN_REDIRECT_URL = 'buku'
 LOGIN_URL = 'masuk'
